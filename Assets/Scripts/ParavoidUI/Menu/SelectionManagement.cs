d904e6bc5c29b27908f706f28e5c6d02e6e2a747
Ryan <ryanabusa8134@gmail.com>
2021-09-20 19:08:37 -0400

    Update SelectionManagement.cs

--- Assets/Scripts/ParavoidUI/Menu/SelectionManagement.cs
+++ Assets/Scripts/ParavoidUI/Menu/SelectionManagement.cs
@@ -3,37 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SelectionManagement : MonoBehaviour
+namespace ParavoidUI
 {
-    /*  Notes For this Script:
-    *   
-    */
-
-    public Toggle[] initialToggles;
-
-    public void Awake()
+    public class SelectionManagement : MonoBehaviour
     {
-        if(initialToggles.Length > 0)
-            foreach (Toggle toggle in initialToggles)
-                toggle.isOn = true;
-    }
+        /*  Notes For this Script:
+        *   Intended to automate activation of a specfied gameobject UI componenet
+        *   and deactive the rest under the same parent gameobject 
+        *   (which this script must be attached to do so)
+        */
+
+        public Toggle[] initialToggles;
+
+        public void Awake()
+        {
+            if(initialToggles.Length > 0)
+                foreach (Toggle toggle in initialToggles)
+                    toggle.isOn = true;
+        }
     
 
-    public void ActivateGameObjectOnly(GameObject obj)
-    {
-        foreach (Transform child in transform)
-            child.gameObject.SetActive(obj == child.gameObject ? true : false);
-    }
+        public void ActivateGameObjectOnly(GameObject obj)
+        {
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(obj == child.gameObject ? true : false);
+        }
 
-    public void ActivateToggleComponentOnly(GameObject obj)
-    {
-        foreach (Transform child in transform)
-            child.gameObject.GetComponent<Toggle>().SetIsOnWithoutNotify(obj == child.gameObject ? true : false);
-    }
+        public void ActivateToggleComponentOnly(GameObject obj)
+        {
+            foreach (Transform child in transform)
+                child.gameObject.GetComponent<Toggle>().SetIsOnWithoutNotify(obj == child.gameObject ? true : false);
+        }
 
-    private void DeactivateGameObject(GameObject obj)
-    {
-        obj.SetActive(false);
-    }
+        private void DeactivateGameObject(GameObject obj)
+        {
+            obj.SetActive(false);
+        }   
 
+    }
 }
+
+
