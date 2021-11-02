63657c5ad49494956545ada16dde99bbfeea40a2
OhNarts <46729536+OhNarts@users.noreply.github.com>
2021-11-02 00:23:00 -0400

    Improved slow time
    Slow time now gives the illusion that everything but the player has slowed down by half. Slow time now also has a cooldown and length.

--- Assets/Scripts/SlowTime.cs
+++ Assets/Scripts/SlowTime.cs
@@ -2,39 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlowTime : MonoBehaviour
+namespace Fragsurf.Movement
 {
-    public float timeMultiplier = 0.5f;
-    public bool enabled = false;
-
-    private bool slow = false;
-
-    private float regTime;
-    private float regFixedTime;
-
-    // Start is called before the first frame update
-    void Start()
+    public class SlowTime : MonoBehaviour
     {
-        regTime = Time.timeScale;
-        regFixedTime = Time.fixedDeltaTime;
-    }
+        public float timeMultiplier = 0.5f;
+        public float slowTimeLength = 5.0f;
+        public float slowTimeCooldown = 5.0f;
+        public bool enabled = false;
 
-    // Update is called once per frame
-    void Update()
-    {   if(enabled) {
-            //Designed so that the time scale stuff doesn't get called literally every frame
-            //Probably need to change the input but IDK how that works
-            if(Input.GetMouseButton(2) && !slow){
-                Time.timeScale = regTime * timeMultiplier;
-                Time.fixedDeltaTime = regFixedTime * timeMultiplier;
-                slow = true;
-            }
-            else if (!Input.GetMouseButton(2) && slow) {
+        private bool slow = false;
+        private bool hasSlowed = false;
+        private float startSlowTime = 0f;
+        private float startCooldown = 0f;
 
-                Time.timeScale = regTime;
-                Time.fixedDeltaTime = regFixedTime;
-                slow = false;
+        private MovementConfig moveConfig;
+        private MoveData _moveData;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SurfCharacter surf = transform.GetComponent<SurfCharacter>();
+            moveConfig = surf.moveConfig;
+            _moveData = surf.MoveData;
+        }
+
+        void FixedUpdate()
+        {   
+            if(enabled) 
+            {
+                float diffSlowTime = Time.time - startSlowTime;
+                float diffCooldown;
+                
+                //Here so that the player can use the slow time function at time = 0
+                if(!hasSlowed)
+                {
+                    diffCooldown = slowTimeCooldown;
+                }
+                else
+                {
+                    diffCooldown = Time.time - startCooldown;
+                }
+
+                //Placeholder right click to activate the time slow
+                if(Input.GetMouseButton(1) && !slow && diffCooldown >= slowTimeCooldown)
+                {
+                    ApplyTimeMult(timeMultiplier);
+                    startSlowTime = Time.time;
+                    slow = true;
+                    hasSlowed = true;
+                }
+                else if (diffSlowTime >= slowTimeLength && slow)
+                {
+                    ApplyTimeMult(1/timeMultiplier);
+                    startCooldown = Time.time;
+                    slow = false;
+                }
             }
+        }
+
+        private void ApplyTimeMult(float mult)
+        {
+            Time.timeScale *= mult;
+            Time.fixedDeltaTime *= mult;
+
+
+            //IDK entirely why, but this set of movement stuff seems to work in making the player feel like they're not slowing down
+            moveConfig.Friction /= mult;
+            moveConfig.AirAccel /= mult;
+            moveConfig.Gravity /= mult;
+            moveConfig.JumpHeight /= mult;
+            moveConfig.JumpPower /= mult;
+            moveConfig.Accel /= mult;
+
+            _moveData.WalkFactor /= mult;
+            _moveData.GravityFactor /= mult;
+            _moveData.Velocity /= mult;
         }
     }
 }
+
