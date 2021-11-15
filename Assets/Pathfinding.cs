0c946d22f9425a2ff71acf931247635c58763075
averylg <71529955+averylg@users.noreply.github.com>
2021-11-15 09:45:40 -0500

    Minor code cleanup

--- Assets/Pathfinding.cs
+++ Assets/Pathfinding.cs
@@ -1,98 +1,98 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.AI;
-// using Paravoid.DataStructures;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Paravoid.DataStructures;
 
-// [RequireComponent(typeof(NavMeshAgent))]
-// public class Pathfinding : MonoBehaviour
-// {
-//     NavMeshAgent navMeshAgent;
-//     public GameObject waypoints;
-//     private WaypointNode goal;
-//     private int currWaypoint;
+[RequireComponent(typeof(NavMeshAgent))]
+public class Pathfinding : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public GameObject waypoints;
+    private WaypointNode goal;
+    private int currWaypoint;
 
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         navMeshAgent = GetComponent<NavMeshAgent>();
-//         currWaypoint = -1;
-//         setNextWaypoint();
-//     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        currWaypoint = -1;
+        setNextWaypoint();
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance == 0)
-//         {
-//             setNextWaypoint();
-//         }
-//     }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance == 0)
+        {
+            setNextWaypoint();
+        }
+    }
 
-//     private void setNextWaypoint()
-//     {
-//         if (waypoints.Length == 0)
-//         {
-//             Debug.LogWarning("No waypoints found");
-//         }
+    private void setNextWaypoint()
+    {
+        if (waypoints.GetComponentsInChildren<GameObject>().Length == 0)
+        {
+            Debug.LogWarning("No waypoints found");
+        }
 
-//         if (currWaypoint >= waypoints.Length)
-//         {
-//             currWaypoint = -1;
-//         }
-//         else
-//         {
-//             currWaypoint++;
-//         }
+        if (currWaypoint >= waypoints.GetComponentsInChildren<GameObject>().Length)
+        {
+            currWaypoint = -1;
+        }
+        else
+        {
+            currWaypoint++;
+        }
 
-//         navMeshAgent.SetDestination(waypoints[currWaypoint].transform.position);
-//     }
+        navMeshAgent.SetDestination(waypoints.GetComponentInChildren<WaypointNode>().transform.position);
+    }
 
-//     void Astar(WaypointNode start)
-//     {
-//         HashSet<WaypointNode> closed = new HashSet<WaypointNode>();
-//         PriorityQueue<WaypointNode, float> open = new PriorityQueue<WaypointNode, float>();
-//         open.Enqueue(start, 0);
-//         WaypointNode curr = start;
+    void Astar(WaypointNode start)
+    {
+        HashSet<WaypointNode> closed = new HashSet<WaypointNode>();
+        PriorityQueue<WaypointNode, float> open = new PriorityQueue<WaypointNode, float>();
+        open.Enqueue(start, 0);
+        WaypointNode curr = start;
 
-//         while (!isGoal(curr) && open.Count > 0)
-//         {
-//             // mark current node as visited
-//             closed.Add(current);
+        while (!IsGoal(curr) && open.Count > 0)
+        {
+            // mark current node as visited
+            closed.Add(curr);
 
-//             // add neighbors to open queue, ranked by cost (g + h)
-//             foreach (GameObject node in curr.NodeMap.Keys)
-//             {
-//                 // run heuristic function
-//                 float h = heuristic(node);
-//                 open.Enqueue(node, curr.NodeMap[node.gameObject] + h);
-//             }
+            // add neighbors to open queue, ranked by cost (g + h)
+            foreach (GameObject node in curr.NodeMap.Keys)
+            {
+                // run heuristic function
+                float h = Heuristic(node.GetComponent<WaypointNode>());
+                open.Enqueue(node.GetComponent<WaypointNode>(), curr.NodeMap[node.gameObject] + h);
+            }
 
-//             open.Dequeue();
+            open.Dequeue();
 
-//             curr = open.Dequeue();
-//         }
+            curr = open.Dequeue();
+        }
 
-//         if (isGoal(curr))
-//         {
-//             //reconstruct solution
-//             // double check how to do this fo rmoving goal
-//         }
-//         else
-//         {
-//             // switch back to random nav
-//         }
+        if (IsGoal(curr))
+        {
+            //reconstruct solution
+            // double check how to do this fo rmoving goal
+        }
+        else
+        {
+            // switch back to random nav
+        }
 
-//     }
+    }
 
-//     void setGoal(WaypointNode targ) { goal = targ; }
+    void SetGoal(WaypointNode targ) { goal = targ; }
 
-//     bool isGoal(WaypointNode targ) { return targ == goal; }
+    bool IsGoal(WaypointNode targ) { return targ == goal; }
 
-//     float heuristic(WaypointNode node)
-//     {
-//         return 0f;
-//     }
-// }
+    float Heuristic(WaypointNode node)
+    {
+        return 0f;
+    }
+}
