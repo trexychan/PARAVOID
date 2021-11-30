f46ee5688a49784e01092020138110622c3c4b54
Ryan <ryanabusa8134@gmail.com>
2021-11-29 20:55:47 -0500

    YEAHHH IT DOES WHAT IT DOES!!!!!

--- Assets/Scripts/ParavoidUI/SceneLoader.cs
+++ Assets/Scripts/ParavoidUI/SceneLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DataManagement;
 
 namespace ParavoidUI
 {
@@ -9,8 +10,14 @@ namespace ParavoidUI
     {
         public static void LoadScene(string scene)
         {
+            PlayerCarryOverData.UpdatePlayerData(GameObject.Find("Player").GetComponent<Player>());
             SceneManager.LoadSceneAsync(scene);
             Time.timeScale = 1f;
+        }
+
+        public void OnTriggerEnter(Collider collider)
+        {
+            LoadScene("MazeLevelMaster");
         }
     }
 }
